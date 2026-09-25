7c8c989e430a4af9f0af8f2825144fdfd328447c
paprika <>
2026-09-25 12:25:07 +0200

    add folder / package structure

---
+++ src/main/java/be/pa_pri_ka/Rube_Goldberg_Portofolio/RubeGoldbergPortofolioApplication.java
@@ -0,0 +1,13 @@
+package be.pa_pri_ka.Rube_Goldberg_Portofolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubeGoldbergPortofolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubeGoldbergPortofolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/pa_pri_ka/Rube_Goldberg_Portofolio/RubeGoldbergPortofolioApplicationTests.java
@@ -0,0 +1,13 @@
+package be.pa_pri_ka.Rube_Goldberg_Portofolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubeGoldbergPortofolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
